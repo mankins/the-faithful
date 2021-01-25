05e47fdb5b1ff99beb0762c8dd74717c8b0a7547
Matt Mankins <mankins@gmail.com>
2021-01-25 00:04:35 +0000

    init

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: ["./src/**/*.svelte", "./static/*.html"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      // colors: {
+      //   cyan: colors.violet,
+      // }
+    },
+  },
+  variants: {
+    extend: {
+    },
+    opacity: ({ after }) => after(['disabled']),
+    typography: ["responsive", "dark"],
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('@tailwindcss/forms'),
+//    require("tailwindcss-font-inter")(),
+  ],
+};
+
