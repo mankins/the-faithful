a13e0a6ef9d138e402b422bf30d7eb04e8397ad4
Matt Mankins <mankins@gmail.com>
2021-02-12 17:53:31 +0000

    fix path

--- functions/fn/transcode/index.js
+++ functions/fn/transcode/index.js
@@ -1,4 +1,4 @@
-//const path = require('path');
+const path = require('path');
 const functions = require('firebase-functions');
 
 const { createTranscodeJob } = require('../../lib/transcode');
@@ -6,13 +6,12 @@ const { createTranscodeJob } = require('../../lib/transcode');
 exports.transcoderIn = functions.storage
   .bucket('video.the-faithful.com')
   .object()
-    .onFinalize(async (object) => {
-      
-        console.log({ object });
+  .onFinalize(async (object) => {
+    console.log({ object });
 
     const fileBucket = object.bucket; // The Storage bucket that contains the file.
     const filePath = object.name; // File path in the bucket.
-    // const fileDir = path.dirname(filePath);
+    const fileDir = path.dirname(filePath);
     const contentType = object.contentType; // File content type.
     // const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
     // const fileName = path.basename(filePath);
@@ -26,11 +25,19 @@ exports.transcoderIn = functions.storage
     }
 
     // if we're here, let's kick off a transcoder job
-      const projectId = 'the-faithful';
-      const location = 'us-central1';
-      const inputUri = `gs://${fileBucket}/${filePath}`;
-      const outputUri = `gs://${fileBucket}/${filePath.replace('input/','output/')}`;
-      
-        const result = await createTranscodeJob({ projectId, location, inputUri, outputUri });
-      console.log(result);
+    const projectId = 'the-faithful';
+    const location = 'us-central1';
+    const inputUri = `gs://${fileBucket}/${filePath}`;
+    const outputUri = `gs://${fileBucket}/${fileDir.replace(
+      'input/',
+      'output/'
+    )}`;
+
+    const result = await createTranscodeJob({
+      projectId,
+      location,
+      inputUri,
+      outputUri,
+    });
+    console.log(result);
   });
