194db12fd1ac160236cc8300c028dd29fa94ca26
Matt Mankins <mankins@gmail.com>
2021-02-15 12:43:19 +0000

    format privacy

--- src/routes/legal/privacy.svelte
+++ src/routes/legal/privacy.svelte
@@ -1,16 +1,75 @@
 <main>
-    <div class="p-20 text-md dark:text-gray-200 text-gray-900 max-w-prose">
-       <h2
-          class="mt-4 mb-4 text-4xl tracking-tight font-extrabold sm:mt-5 sm:leading-none lg:mt-6 lg:text-5xl xl:text-6xl"> 
-Privacy Policy</h2>
-<p class="mb-4">Your privacy is important to us. It is The Faithful, LLC's policy to respect your privacy regarding any information we may collect from you across our website, <a href="https://www.the-faithful.com/">https://www.the-faithful.com</a>, and other sites we own and operate.</p>
-<p class="mb-4">We only ask for personal information when we truly need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.</p>
-<p class="mb-4">We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorized access, disclosure, copying, use or modification.</p>
-<p class="mb-4">We don’t share any personally identifying information publicly or with third-parties, except when required to by law.</p>
-<p class="mb-4">Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
-<p class="mb-4">You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.</p>
-<p class="mb-4">Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.</p>
-<p class="mb-4">This policy is effective as of 11 January 2021.</p>
-<p class="mb-4"><a href="https://getterms.io" title="Generate a free privacy policy">Privacy Policy created with GetTerms.</a></p>
-</div>
+  <div class="p-20 text-md dark:text-gray-200 text-gray-900 max-w-prose">
+    <h2
+      class="mt-4 mb-4 text-4xl tracking-tight font-extrabold sm:mt-5 sm:leading-none lg:mt-6 lg:text-5xl xl:text-6xl"
+    >
+      Privacy Policy
+    </h2>
+    <p class="mb-4">
+      Your privacy is important to us. It is The Faithful, LLC's policy to
+      respect your privacy regarding any information we may collect from you
+      across our website, <a href="https://www.the-faithful.com/"
+        >https://www.the-faithful.com</a
+      >, and other sites we own and operate.
+    </p>
+    <p class="mb-4">
+      We only ask for personal information when we truly need it to provide a
+      service to you. We collect it by fair and lawful means, with your
+      knowledge and consent. We also let you know why we’re collecting it and
+      how it will be used.
+    </p>
+    <p class="mb-4">
+      We only retain collected information for as long as necessary to provide
+      you with your requested service. What data we store, we’ll protect within
+      commercially acceptable means to prevent loss and theft, as well as
+      unauthorized access, disclosure, copying, use or modification.
+    </p>
+    <p class="mb-4">
+      We don’t share any personally identifying information publicly or with
+      third-parties, except when required to by law.
+    </p>
+    <p class="mb-4">
+      We want to process as little personal information as possible when you use
+      our website. That's why we've chosen Fathom Analytics for our website
+      analytics, which doesn't use cookies and complies with the GDPR, ePrivacy
+      (including PECR), COPPA and CCPA. Using this privacy-friendly website
+      analytics software, your IP address is only briefly processed, and we
+      (running this website) have no way of identifying you. As per the CCPA,
+      your personal information is de-identified. You can read more about this
+      on <a href="https://usefathom.com/compliance" rel="nofollow"
+        >Fathom Analytics' website</a
+      >. The purpose of us using this software is to understand our website
+      traffic in the most privacy-friendly way possible so that we can
+      continually improve our website and business. The lawful basis as per the
+      GDPR is "f); where our legitimate interests are to improve our website and
+      business continually." As per the explanation, no personal data is stored
+      over time.
+    </p>
+    <p class="mb-4">
+      Our website may link to external sites that are not operated by us. Please
+      be aware that we have no control over the content and practices of these
+      sites, and cannot accept responsibility or liability for their respective
+      privacy policies.
+    </p>
+    <p class="mb-4">
+      You are free to refuse our request for your personal information, with the
+      understanding that we may be unable to provide you with some of your
+      desired services.
+    </p>
+    <p class="mb-4">
+      Your continued use of our website will be regarded as acceptance of our
+      practices around privacy and personal information. If you have any
+      questions about how we handle user data and personal information, feel
+      free to contact us.
+    </p>
+    <p class="mb-4">This policy is effective as of 11 January 2021.</p>
+    <p class="mb-4">
+      <a
+        href="https://getterms.io"
+        rel="nofollow"
+        title="Generate a free privacy policy"
+        >Privacy Policy created with GetTerms.</a
+      >
+    </p>
+  </div>
 </main>
