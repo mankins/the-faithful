e899b31fc62d0ea0b7721ea39f14e9a27f6f6384
Matt Mankins <mankins@gmail.com>
2021-02-17 18:49:22 +0000

    mobile nav menu

--- src/components/nav/Nav.svelte
+++ src/components/nav/Nav.svelte
@@ -6,17 +6,19 @@
   export let user = {};
   export let loggedIn = false;
 
-  let section = "home";
+  let section = 'home';
+  let menuOpen = false;
 
-  onMount(()=> {
+  onMount(() => {
+    if (window.location.href.indexOf('schedule') !== -1) {
+      section = 'schedule';
+    }
+    if (window.location.href.indexOf('preview') !== -1) {
+      section = 'watch';
+    }
 
-if (window.location.href.indexOf('schedule') !== -1) {
-    section = "schedule";
-}
-console.log({section});
+ //   console.log({ section });
   });
-  
-
 </script>
 
 <nav class="bg-white fixed w-screen">
@@ -26,18 +28,18 @@ console.log({section});
         <div class="-ml-2 mr-2 flex items-center md:hidden">
           <!-- Mobile menu button -->
           <button
+            on:click={() => {
+              menuOpen = !menuOpen;
+            }}
             class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-faithful-500"
             aria-expanded="false"
           >
             <span class="sr-only">Open main menu</span>
             <!-- Icon when menu is closed. -->
-            <!--
-                Heroicon name: outline/menu
-  
-                Menu open: "hidden", Menu closed: "block"
-              -->
             <svg
-              class="block h-6 w-6"
+              class:block={!menuOpen}
+              class:hidden={menuOpen}
+              class="h-6 w-6"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -52,13 +54,10 @@ console.log({section});
               />
             </svg>
             <!-- Icon when menu is open. -->
-            <!--
-                Heroicon name: outline/x
-  
-                Menu open: "block", Menu closed: "hidden"
-              -->
             <svg
-              class="hidden h-6 w-6"
+              class:block={menuOpen}
+              class:hidden={!menuOpen}
+              class="h-6 w-6"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -75,18 +74,18 @@ console.log({section});
           </button>
         </div>
         <div class="flex-shrink-0 flex items-center">
-            <a href="/" title="The Faithful Home">
-          <img
-            class="block lg:hidden h-8 w-auto"
-            src="/svg/logo.svg"
-            alt="Workflow"
-          />
-          <img
-            class="hidden lg:block h-8 w-auto"
-            src="/svg/logo.svg"
-            alt="Workflow"
-          />
-        </a>
+          <a href="/" title="The Faithful Home">
+            <img
+              class="block lg:hidden h-8 w-auto"
+              src="/svg/logo.svg"
+              alt="The Faithful"
+            />
+            <img
+              class="hidden lg:block h-8 w-auto"
+              src="/svg/logo.svg"
+              alt="The Faithful"
+            />
+          </a>
         </div>
         <div class="hidden md:ml-6 md:flex md:space-x-8">
           <a
@@ -98,21 +97,11 @@ console.log({section});
             Home
           </a>
           <a
-            href="#"
-            class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-            class:border-b-4={section === 'tickets'}
-            class:border-faithful-500={section === 'tickets'}
-
-            >
-            Tickets
-          </a>
-          <a
             href="/trailer"
             class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
             class:border-b-4={section === 'trailer'}
             class:border-faithful-500={section === 'trailer'}
-
-            >
+          >
             Watch
           </a>
           <a
@@ -120,15 +109,14 @@ console.log({section});
             class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
             class:border-b-4={section === 'schedule'}
             class:border-faithful-500={section === 'schedule'}
-
-            >
+          >
             Schedule
           </a>
         </div>
       </div>
       <div class="flex items-center">
         <div class="flex-shrink-0">
-            <slot></slot>
+          <slot />
         </div>
         <div class="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center">
           {#if notifications}
@@ -154,9 +142,9 @@ console.log({section});
               </svg>
             </button>
           {/if}
-          <!-- Profile dropdown -->
           {#if false}
-            <div class="ml-3 relative">
+          <!-- Profile dropdown -->
+          <div class="ml-3 relative">
               {#if loggedIn}
                 <NavUserMenu {user} />
               {:else}
@@ -174,87 +162,33 @@ console.log({section});
     </div>
   </div>
 
-  <!--
-      Mobile menu, toggle classes based on menu state.
-  
-      Menu open: "block", Menu closed: "hidden"
-    -->
-  <div class="hidden md:hidden">
+  <div
+    class:block={menuOpen}
+    class:hidden={!menuOpen}
+    class="md:hidden bg-gray-50"
+  >
     <div class="pt-2 pb-3 space-y-1">
-      <!-- Current: "bg-faithful-50 border-faithful-500 text-faithful-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
       <a
-        href="#"
-        class="bg-faithful-50 border-faithful-500 text-faithful-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
-        >Dashboard</a
+        href="/"
+        class="block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+        class:border-faithful-500={section === 'home'}
+        class:text-faithful-700={section === 'home'}
+        >Home</a
       >
       <a
-        href="#"
+        href="/trailer"
         class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
-        >Team</a
+        class:border-faithful-500={section === 'watch'}
+        class:text-faithful-700={section === 'watch'}
+        >Watch</a
       >
       <a
-        href="#"
+        href="/schedule"
         class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
-        >Projects</a
+        class:border-faithful-500={section === 'schedule'}
+        class:text-faithful-700={section === 'schedule'}
+        >Schedule</a
       >
-      <a
-        href="#"
-        class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
-        >Calendar</a
-      >
-    </div>
-    <div class="pt-4 pb-3 border-t border-gray-200">
-      <div class="flex items-center px-4 sm:px-6">
-        <div class="flex-shrink-0">
-          <img
-            class="h-10 w-10 rounded-full"
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=Z7yVziSUAk&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            alt=""
-          />
-        </div>
-        <div class="ml-3">
-          <div class="text-base font-medium text-gray-800">Tom Cook</div>
-          <div class="text-sm font-medium text-gray-500">tom@example.com</div>
-        </div>
-        <button
-          class="ml-auto flex-shrink-0 bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-faithful-500"
-        >
-          <span class="sr-only">View notifications</span>
-          <!-- Heroicon name: outline/bell -->
-          <svg
-            class="h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-            />
-          </svg>
-        </button>
-      </div>
-      <div class="mt-3 space-y-1">
-        <a
-          href="#"
-          class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
-          >Your Profile</a
-        >
-        <a
-          href="#"
-          class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
-          >Settings</a
-        >
-        <a
-          href="#"
-          class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 sm:px-6"
-          >Sign out</a
-        >
-      </div>
     </div>
   </div>
 </nav>
