6073cd432a1e330ae6172569832f95c423494438
Matt Mankins <mankins@gmail.com>
2021-03-19 20:51:17 +0000

    change redirect

--- src/routes/my/theatre/index.js
+++ src/routes/my/theatre/index.js
@@ -3,7 +3,7 @@ export async function get(req, res) {
     return Promise.resolve({
         status: 302,
         headers: {
-            "location": '/my/theatre/waiting',
+            "location": '/my/theatre/theatre',
             "cache-control": "no-cache; max-age=30",
         },
         body: ''
