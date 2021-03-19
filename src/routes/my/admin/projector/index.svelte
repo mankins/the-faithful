fef7c537e5b1175fa5079ee1e4b3d2126211c1b5
Matt Mankins <mankins@gmail.com>
2021-03-19 16:14:26 +0000

    change start time

--- src/routes/my/admin/projector/index.svelte
+++ src/routes/my/admin/projector/index.svelte
@@ -286,7 +286,7 @@
             <button
               type="button"
               on:click={() => {
-                setTime(10);
+                setTime(5);
               }}
               class="ml-3 relative inline-flex items-center px-4 py-2 rounded-l-md rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
             >
