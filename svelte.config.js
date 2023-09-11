42b47c11fa67e17c5627199569506456c70cb11d
Matt Mankins <mankins@gmail.com>
2023-09-11 18:42:32 +0200

    chore: upgrades and node apterS

--- svelte.config.js
+++ svelte.config.js
@@ -76,7 +76,8 @@
 // };
 // export default config;
 
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 // import adapter from '@sveltejs/adapter-vercel';
 // import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
